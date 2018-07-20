45a0f7a20141564e561dc8ed7d89a089f8b97ca3
Koen Klaren <koen.klaren@gmail.com>
2018-07-20 13:32:03 +0200

    Add `width` prop to SerpPreview (#1)
    * Add `width` prop to SerpPreview
    
    * Update README.md
    
    * Add width input to demo

--- docs/index.js
+++ docs/index.js
@@ -13,11 +13,13 @@ class Demo extends React.Component {
             metaDescription:
                 'Example Domain. This domain is established to be used for illustrative examples in documents. You may use this domain in examples without prior coordination or asking for permission. – Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rutrum sed eros iaculis congue. Aliquam erat volutpat. Aenean tortor sapien, porta ac interdum vel, interdum sed sapien.',
             url: 'https://example.com/',
+            width: 600,
         };
 
         this.changeTitle = this.changeTitle.bind(this);
         this.changeMetaDescription = this.changeMetaDescription.bind(this);
         this.changeUrl = this.changeUrl.bind(this);
+        this.changeWidth = this.changeWidth.bind(this);
     }
 
     changeTitle(e) {
@@ -35,6 +37,12 @@ class Demo extends React.Component {
     changeUrl(e) {
         this.setState({
             url: e.target.value,
+        });
+    }
+
+    changeWidth(e) {
+        this.setState({
+            width: parseInt(e.target.value, 10),
         });
     }
 
@@ -57,11 +65,12 @@ class Demo extends React.Component {
                         </a>
                     </div>
                 </div>
-                <div className="preview">
+                <div className="preview" style={{ width: this.state.width }}>
                     <SerpPreview
                         title={this.state.title}
                         metaDescription={this.state.metaDescription}
                         url={this.state.url}
+                        width={this.state.width}
                     />
                 </div>
                 <div className="options">
@@ -91,6 +100,18 @@ class Demo extends React.Component {
                             type="text"
                             value={this.state.url}
                             onChange={this.changeUrl}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="width">Width</label>
+                        <input
+                            id="width"
+                            type="number"
+                            step="50"
+                            min="300"
+                            max="800"
+                            value={this.state.width}
+                            onChange={this.changeWidth}
                         />
                     </div>
                 </div>
